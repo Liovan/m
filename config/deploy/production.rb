5c3bf6ceceaed00e9d984d240e759c29918f39d9
Liovan <li0van@yahoo.com>
2017-04-01 11:34:20 +0430

    hello world

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :stage, :production
-server '138.201.178.14', user: 'deploy', roles: %w{app db web}
+server '5.160.247.104', user: 'deploy', roles: %w{app db web}
 
-
+ 
 # role-based syntax
 # ==================
 
